e8c549f06a9bcb75a73cf26a360ca3f49d9a7d11
Chanson <chansonpro@outlook.com>
2017-04-26 19:39:47 +0800

    张松测试用的test程序

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-04-26 19:20:27
+ * @version $Id$
+ */
+
+class ClassName extends AnotherClass {
+    
+
+    function getAcess(){
+    	$name = '123';
+    	return  $name;
+    }
+}
---
+++ test2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-04-26 19:35:52
+ * @version $Id$
+ */
+function zsong(){
+	
+}
+        
+
